b0b7ed94fa0cbb9946f20e61180a650de830ce80
rishabh-jain89 <jainrishabh2005@gmail.com>
2025-07-03 19:46:03 +0530

    Updated codes

--- testing_gender_classification.py
+++ testing_gender_classification.py
@@ -1,3 +1,4 @@
+# Importing Libraries and Modules
 import os
 import numpy as np
 from PIL import Image
@@ -8,57 +9,70 @@ from pathlib import Path
 
 # Set image size and model path
 IMG_SIZE = (224, 224)
-MODEL_PATH = "gender_recognition.h5"
+MODEL_PATH = "gender_classification.h5"
 
 # Load the trained model
 model = load_model(MODEL_PATH)
 
+#Function to load and resize Images
 def load_data(file_path):
     images = []
-    name = []
 
     for img in sorted(os.listdir(file_path)):
         img_path = os.path.join(file_path, img)
-        image = Image.open(img_path).convert("RGB")
-        image = image.resize(IMG_SIZE)
+        image = Image.open(img_path).convert("RGB") # Converting images to RGB format
+        image = image.resize(IMG_SIZE) # Resizing Images
         images.append(image)
-        name.append(img)
 
-    return images, name
+    return images
 
+# Taking Path as Input
 path_val = input("Enter validation Path:")
 path_val = Path(path_val)
 
+# Splitting Male and Female paths
 val_file_path_male = os.path.join(path_val, "male")
 val_file_path_female = os.path.join(path_val, "female")
 
+# Loading Male and Female Images Separately
+test_male_set = load_data(val_file_path_male)
+test_female_set = load_data(val_file_path_female)
 
-test_male_set, test_male_name = load_data(val_file_path_male)
+# Making a list of Male and Female Labels
 test_male_label = [1] * len(test_male_set)
-test_male_label = np.array(test_male_label)
-test_female_set, test_female_name = load_data(val_file_path_female)
 test_female_label = [0] * len(test_female_set)
-test_female_label = np.array(test_female_label)
 
+# Converting labels and set into numpy array
+test_male_label = np.array(test_male_label)
+test_female_label = np.array(test_female_label)
 test_male_set = np.stack(test_male_set)
 test_female_set = np.stack(test_female_set)
+
+# Combining Male + Female data
 test_set = np.concatenate((test_male_set, test_female_set))
 test_label =  np.concatenate((test_male_label, test_female_label))
 
-
+# Normalizing Images
 test_set = test_set/255
 
+# Making prediction on test set using loaded model
 preds = model.predict(test_set)
 preds_binary = (preds > 0.5).astype(int).flatten()
 
+# Calculating Accuracy, Precision, Recall and F1 score
 accuracy = accuracy_score(test_label, preds_binary)
 precision = precision_score(test_label, preds_binary)
 recall = recall_score(test_label, preds_binary)
 f1 = f1_score(test_label, preds_binary)
 
-print(f"Accuracy:  {accuracy:.4f}")
-print(f"Precision: {precision:.4f}")
-print(f"Recall:    {recall:.4f}")
-print(f"F1 Score:  {f1:.4f}")
-cm = confusion_matrix(test_label, preds_binary)
-print(cm)
+# Creating a log string
+log = (
+    f"Accuracy:  {accuracy:.4f}\n"
+    f"Precision: {precision:.4f}\n"
+    f"Recall:    {recall:.4f}\n"
+    f"F1 Score:  {f1:.4f}\n"
+)
+
+# Write to file
+with open("validation_results_gender_classification.txt", "a") as f:
+    f.write(log)
--- training_gender_classification.py
+++ training_gender_classification.py
@@ -1,3 +1,4 @@
+#Importing Libraries and Modules
 import os
 import random
 import numpy as np
@@ -7,166 +8,104 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D,  MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-from sklearn.utils.class_weight import compute_class_weight
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.regularizers import l2
+from tensorflow.keras.callbacks import CSVLogger
 
-train_file_path_male = "/home/rishabh-jain/Desktop/Syscom/New Dataset/Comys_Hackathon5/Task_A/train/male"
-train_file_path_female = "/home/rishabh-jain/Desktop/Syscom/New Dataset/Comys_Hackathon5/Task_A/train/female"
 
-val_file_path_male = "/home/rishabh-jain/Desktop/Syscom/New Dataset/Comys_Hackathon5/Task_A/val/male"
-val_file_path_female = "/home/rishabh-jain/Desktop/Syscom/New Dataset/Comys_Hackathon5/Task_A/val/female"
 
+#Defining dataset Paths
+train_path = "/home/rishabh-jain/Desktop/Syscom/New Dataset/Comys_Hackathon5/Task_A/train"
+train_file_path_male = os.path.join(train_path, "male")
+train_file_path_female = os.path.join(train_path, "female")
+
+#Setting Image size and seed
 img_size = (224, 224)
+random.seed(42)
 
+#Function to load and resize Images
 def load_data(file_path):
     images = []
-    name = []
 
     for img in sorted(os.listdir(file_path)):
         img_path = os.path.join(file_path, img)
-        image = Image.open(img_path).convert("RGB")
-        image = image.resize(img_size)
+        image = Image.open(img_path).convert("RGB") #converting images to RGB format
+        image = image.resize(img_size) #Resizing Images
         images.append(image)
-        name.append(img)
+    return images
 
-    return images, name
+ # Loading Male and Female Images Separately
+train_male_set= load_data(train_file_path_male)
+train_female_set = load_data(train_file_path_female)
 
-train_male_set, train_male_name = load_data(train_file_path_male)
-train_male_label = [1] * len(train_male_set)
-train_male_label = np.array(train_male_label)
-train_female_set, train_female_name = load_data(train_file_path_female)
+# Making a list of Male and Female Labels
+train_male_label = [1] * len(train_male_set) 
 train_female_label = [0] * len(train_female_set)
+
+#Converting labels into numpy array
+train_male_label = np.array(train_male_label)
 train_female_label = np.array(train_female_label)
 
-test_male_set, test_male_name = load_data(val_file_path_male)
-test_male_label = [1] * len(test_male_set)
-test_male_label = np.array(test_male_label)
-test_female_set, test_female_name = load_data(val_file_path_female)
-test_female_label = [0] * len(test_female_set)
-test_female_label = np.array(test_female_label)
-
-print(train_male_label.shape, test_male_label.shape)
-print(train_female_label.shape, test_female_label.shape)
-
+#Defining Data Augmentation Pipeline
 datagen = ImageDataGenerator(
-    rotation_range=45, 
-    width_shift_range=0.25,
-    height_shift_range=0.25,
-    shear_range=0.25,
-    zoom_range=0.3,
-    horizontal_flip=True,
-    vertical_flip=True,  
-    brightness_range=[0.7, 1.4], 
-    channel_shift_range=50,  
-    fill_mode='reflect'  
+    rotation_range=45, # Randomly rotate images by upto 45 degrees
+    width_shift_range=0.25, # Shift image horizontally by up to 25% of the width
+    height_shift_range=0.25, # Shift image vertically by up to 25% of the height
+    shear_range=0.25, #  Apply shearing (tilting) transformations up to 25 degrees
+    zoom_range=0.3, #  Randomly zoom in/out by up to 30%
+    horizontal_flip=True,#   Randomly flip images horizontally (mirror image)
+    vertical_flip=True,  #  Randomly flip images vertically (upside down)
+    brightness_range=[0.7, 1.4], #  Adjust brightness randomly between 70% (darker) and 140% (brighter)
+    channel_shift_range=50,  #  Randomly shift color channels by up to 50 units (simulate lighting changes)
+    fill_mode='reflect'  #  Filling strategy for newly created pixels (reflects pixel values at the border)
 )
 
+# Augment female images to balance dataset
 female_imgs_aug = []
 female_labels_aug = []
 
 for img in train_female_set:  
-    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=0) # expand batch dimensions
     aug_iter = datagen.flow(img, batch_size=1)
-    for i in range(3):  
+    for i in range(3):  # Generate 3 augmentations per image
         aug_img = next(aug_iter)[0]
         female_imgs_aug.append(aug_img)
         female_labels_aug.append(0)
 
+# Stack Augmented data into arrays
 female_imgs_aug = np.stack(female_imgs_aug)
 female_labels_aug = np.array(female_labels_aug)
 
+# Combining Original + Augmented female data
 train_female_set = np.stack(train_female_set)
 train_female_aug_set = np.concatenate((train_female_set, female_imgs_aug))
-
-train_female_label = np.array(train_female_label)
 train_female_aug_label = np.concatenate((train_female_label, female_labels_aug))
 
-print(train_female_aug_set.shape, train_female_aug_label.shape)
-
+# Clip pizel values to vaid range and convert to uint8
 train_female_aug_set = np.clip(train_female_aug_set, 0, 255).astype(np.uint8)
 
-i = random.randint(0, train_female_aug_set.shape[0])
-
-plt.imshow(train_female_aug_set[i])
-plt.axis('off')  # Hide axes
-plt.title(f"Label: {train_female_aug_label[i]} ")
-plt.show()
-
-j = random.randint(0, train_female_set.shape[0])
-
-plt.imshow(train_female_set[j])
-plt.axis('off')  # Hide axes
-plt.title(f"Label: {train_female_label[j]} ")
-plt.show()
-
-def stack_images(images, labels):
-    fig, axes = plt.subplots(4, 5, figsize=(15, 12)) 
-
-    for i, ax in enumerate(axes.flat):
-        if i < len(images):
-            ax.imshow(images[i])
-            ax.set_title(str(labels[i]))   
-            ax.axis('off')                 
-        else:
-            ax.axis('off')                 
-
-    plt.tight_layout()
-    plt.show()
-
+# stack final female data
 train_female_aug_set = np.stack(train_female_aug_set)
 
+# Combining Male and Female data
 train_male_set = np.stack(train_male_set)
 train_set = np.concatenate((train_male_set, train_female_aug_set))
-
 train_label = np.concatenate((train_male_label, train_female_aug_label))
 
-test_male_set = np.stack(test_male_set)
-test_female_set = np.stack(test_female_set)
-test_set = np.concatenate((test_male_set, test_female_set))
-test_label =  np.concatenate((test_male_label, test_female_label))
-
-print(train_set.shape, train_label.shape)
-print(test_set.shape, test_label.shape)
-
+# Shuffling the dataset
 combined = list(zip(train_set, train_label))
-random.seed(42)
 random.shuffle(combined)
 train_set, train_label = zip(*combined)
-
-combined = list(zip(test_set, test_label))
-random.shuffle(combined)
-test_set, test_label = zip(*combined)
-
 train_set = np.stack(train_set)
 train_label = np.stack(train_label)
 
-test_set = np.stack(test_set)
-test_label = np.stack(test_label)
-
-print(type(train_set), type(test_set))
-print(type(train_label), type(test_label))
-print(train_set.shape, test_set.shape)
-print(train_label.shape, test_label.shape)
-
-stack_images(train_set, train_label)
-
-weights = compute_class_weight('balanced', classes=np.unique(train_label), y=train_label)
-class_weights = {0: weights[0], 1: weights[1]}
-
+# Normalizing Images
 train_set = (train_set )/255
-test_set = (test_set )/255
 
-unique, counts = np.unique(train_label, return_counts=True)
-print("Train label distribution:", dict(zip(unique, counts)))
-
-unique, counts = np.unique(test_label, return_counts=True)
-print("Test label distribution:", dict(zip(unique, counts)))
-
+#Checking for GPU Availability
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+# Defining CNN Model Architecture
 inp_shape = (224, 224, 3)
 model = Sequential()
 
@@ -193,26 +132,14 @@ model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.001)))
 model.add(Dropout(0.3))
 model.add(Dense(1, activation='sigmoid'))  
 
+# Compiling the Model
 model.compile(loss = tf.keras.losses.BinaryCrossentropy(label_smoothing = 0.1), optimizer = 'adam', metrics = ['accuracy'])
 
-model.fit(train_set, train_label, epochs = 20, batch_size = 64)
+# Setting up logger before training
+csv_logger = CSVLogger('training_results.txt', append=False)
 
-model.evaluate(test_set, test_label, return_dict = True, batch_size = 64)
+# Training the Model
+model.fit(train_set, train_label, epochs = 30, batch_size = 64, callbacks = [csv_logger])
 
-# Predict on full test set
-preds = model.predict(test_set)
-preds_binary = (preds > 0.5).astype(int).flatten()
-
-accuracy = accuracy_score(test_label, preds_binary)
-precision = precision_score(test_label, preds_binary)
-recall = recall_score(test_label, preds_binary)
-f1 = f1_score(test_label, preds_binary)
-
-print(f"Accuracy:  {accuracy:.4f}")
-print(f"Precision: {precision:.4f}")
-print(f"Recall:    {recall:.4f}")
-print(f"F1 Score:  {f1:.4f}")
-cm = confusion_matrix(test_label, preds_binary)
-print(cm)
-
-model.save("gender_recognition.h5")
+# Save the Trained Model
+model.save("gender_classification.h5")
